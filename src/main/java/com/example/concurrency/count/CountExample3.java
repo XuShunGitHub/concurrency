681f4fc051f7fe9f133087183a19a421a82c66d2
XuShunGitHub <505232797@qq.com>
2018-11-27 23:36:33 +0800

    synchronized used

---
+++ src/main/java/com/example/concurrency/count/CountExample3.java
@@ -0,0 +1,43 @@
+package com.example.concurrency.count;
+
+import com.example.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class CountExample3 {
+
+    private static int clientTotal = 5000;
+    private static int threadTotal = 50;
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error(e.getMessage(), e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count: " + count);
+    }
+
+    private synchronized static void add(){
+        count++;
+    }
+}
